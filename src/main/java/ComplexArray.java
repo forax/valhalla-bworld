3e7ac6d88ad19670df8f5fbe3532b0efb0058f75
forax <forax@univ-mlv.fr>
2026-02-04 17:01:48 +0100

    add a call to Array.newInstance new overload

--- src/main/java/ComplexArray.java
+++ src/main/java/ComplexArray.java
@@ -2,6 +2,11 @@ void main() {
   value record Complex(float re, float im) {}
 
   var array = new Complex![] { new Complex(1, 2) };
-  array[0] = null;
+  // array[0] = null;  // NPE!
+
+  var ACC_NULL_CHECKED = 0X200;
+  var copy = (Complex[]) Array.newInstance(Complex.class, ACC_NULL_CHECKED, 1,
+      array, 0);
+  // copy[0] = null;  // NPE!
 }
 
